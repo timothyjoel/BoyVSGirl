4e0307720b2cc8ad21ecddb8a8348f71f478a742
Tymoteusz Stokarski <tjoelstokarski@gmail.com>
2020-09-15 21:36:30 +0200

    Fix - when added first vote it is automatically visible in last votes

--- BoyVSGirl/Home Screen/HomeView.swift
+++ BoyVSGirl/Home Screen/HomeView.swift
@@ -190,29 +190,32 @@ struct HomeLastVoters: View {
     @ObservedObject var vm: HomeViewModel
     
     var body: some View {
+
         ScrollView (.vertical, showsIndicators: false) {
+        if !vm.votes.isEmpty {
             VStack(spacing: 16) {
-                ForEach(vm.votes.reversed(), id: \.id) { vote in
-                    HStack (alignment: .center, spacing: 16) {
-                        CustomImage(image: .avatar)
-                            .frame(width: 45, height: 45)
-                            .padding(.leading, 16)
-                        VStack (alignment: .leading) {
-                            Text(vote.voter)
-                                .font(.system(size: 16, weight: .bold, design: .rounded))
-                                .foregroundColor(.darkBlue)
-                            Text(self.vm.formatted(vote.date))
-                                .font(.system(size: 13, weight: .bold, design: .rounded))
-                                .foregroundColor(.lightGrey)
-                        }
-                        Spacer()
-                        CustomImage(image: .gender)
-                            .foregroundColor(.darkGrey)
-                            .frame(width: 45, height: 45)
-                            .padding(.trailing, 16)
-                        }
-                    .frame(width: UIScreen.width-48, height: 80)
-                    .background(RoundedCorners(color: .superLightGrey, tl: 16, tr: 16, bl: 16, br: 16))
+                    ForEach(vm.votes.reversed(), id: \.id) { vote in
+                        HStack (alignment: .center, spacing: 16) {
+                            CustomImage(image: .avatar)
+                                .frame(width: 45, height: 45)
+                                .padding(.leading, 16)
+                            VStack (alignment: .leading) {
+                                Text(vote.voter)
+                                    .font(.system(size: 16, weight: .bold, design: .rounded))
+                                    .foregroundColor(.darkBlue)
+                                Text(self.vm.formatted(vote.date))
+                                    .font(.system(size: 13, weight: .bold, design: .rounded))
+                                    .foregroundColor(.lightGrey)
+                            }
+                            Spacer()
+                            CustomImage(image: .gender)
+                                .foregroundColor(.darkGrey)
+                                .frame(width: 45, height: 45)
+                                .padding(.trailing, 16)
+                            }
+                        .frame(width: UIScreen.width-48, height: 80)
+                        .background(RoundedCorners(color: .superLightGrey, tl: 16, tr: 16, bl: 16, br: 16))
+                    }
                 }
             }
             
